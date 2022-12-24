ccfa3243d6df8298389c3b866be803ae20ff5cb8
Shoaib Shaikh <shoaibashk.2000@gmail.com>
2022-12-24 20:21:06 +0530

    Added Primitive types

--- 01-getting-started/main.go
+++ 01-getting-started/main.go
@@ -6,18 +6,29 @@ import (
 
 func main() {
 
+	// Primitive data types
 	var i int = 12
 	var j float32 = 12.5
 	var h bool = true
 	var str string = "Welcome"
 	var com complex64 = 1 + 2i
 
+	// Printf in golang
+	fmt.Printf("Type %T Value : %v  \n", i, i)
+	fmt.Printf("Type %T Value : %v  \n", j, j)
+
+	// Println to console
 	fmt.Println("Integer Value = ", i)
 	fmt.Println("Float Value = ", j)
 	fmt.Println("Boolean Value = ", h)
 	fmt.Println("String Value = ", str)
 	fmt.Println("Complex Value = ", com)
-	fmt.Println("Complex Real Number = ", real(com))
-	fmt.Println("Complex Imaginary Number = ", imag(com))
+
+	// multiple assignment
+	re, im := real(com), imag(com)
+
+	
+	fmt.Println("Complex Real Number = ", re)
+	fmt.Println("Complex Imaginary Number = ", im)
 
 }
---
+++ text.t
@@ -0,0 +1,3 @@
+module github[38;5;187m.[0mcom[38;5;210m/[0mShoaibashk[38;5;210m/[0mlearngo
+
+go [38;5;85m1.19[0m
